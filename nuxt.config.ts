b24877125f0fe8ecf269f01c3c2dc972a8ce8a60
Kamil <k.medrala99@gmail.com>
2024-01-02 16:00:02 +0100

    favicon remove, meta improvements

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','@nuxt/image'],
-  app:{
-    head:{
-      meta: [{name:'robots', content:'noindex'}]
-    }
-  }
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  app: {
+    head: {
+      title: 'Ultra Maszyna - Konserwacja',
+      meta: [
+        {
+          name: "robots",
+          content: "noindex",
+        },
+        { "http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate" },
+        { "http-equiv": "Pragma", content: "no-cache" },
+        { "http-equiv": "Expires", content: "0" },
+      ],
+    },
+  },
+});
