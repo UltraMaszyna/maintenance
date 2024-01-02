7a14f2245fb91856d5c6b6bf2c7cdff49ca2609f
Kamil <k.medrala99@gmail.com>
2024-01-02 15:32:49 +0100

    Eveselection maintenance page

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="bg-texture min-h-screen flex items-center justify-center">
-    <ViewUltramaszyna />
+  <div class="bg-texture min-h-screen flex items-center justify-center relative ">
+    <DecorDashesA class="absolute z-10 top-1 md:top-10 left-0 w-64"/>
+    <ViewEveselection />
+    <DecorDashesB class="absolute z-10 bottom-10 right-0 w-64"/>
   </div>
 </template>
 
 <style>
 
 .bg-texture{
-  background-image: url('/bg-texture.png');
+  background-image: url('/bg-texture-cloth.png');
 }
 
 </style>
--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -1,7 +1,7 @@
 <template>
     <a :href="link"
-        class=" block px-6 py-1.5 uppercase text-xl font-bold text-center text-[#4F4F4F] hover:text-[#AA2A89] transition duration-300 svg-hover"
-        :class="{'rounded-full border border-solid border-[#4F4F4F] hover:border-[#AA2A89]':border}"
+        class=" inline-block px-6 py-1.5 uppercase text-xl font-bold text-center text-[#8C6C67] hover:bg-[#8C6C67]/5 transition duration-300 svg-hover"
+        :class="{'rounded-full border border-solid border-[#4F4F4F] hover:border-[#8C6C67]':border}"
         >
         <slot></slot>
     </a>
@@ -22,6 +22,6 @@ defineProps({
 }
 .svg-hover:hover svg path,
 .svg-hover:hover svg circle{
-    @apply !fill-[#AA2A89]
+    @apply !fill-[#8C6C67]
 }
 </style>
